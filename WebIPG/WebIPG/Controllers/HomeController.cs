2a5a8fa3373ee3d3f3242c76b12476261db15a12
SennyWalc <32518041+SennyWalc@users.noreply.github.com>
2018-07-30 11:39:09 +0200

    Zadanie IPG

---
+++ WebIPG/WebIPG/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WebIPG.Models;
+
+namespace WebIPG.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IServicePodstawowa Podstawowa = new ServicePodstawowa();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string GetAll()
+        {
+            try
+            {
+                var podstawowa = Podstawowa.GetAll();
+                var settings = new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() };
+                string elementy = JsonConvert.SerializeObject(podstawowa, Formatting.None, settings);
+                return elementy;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        [HttpPost]
+        public ActionResult PodstawowaEdycja(Podstawowa podstawowa,Podstawowa arch)
+        {
+            bool status = Porownanie(arch);
+            if (status)
+            {
+                Podstawowa.Update(podstawowa);
+                return Json(new { success = true, status = "OK" });
+            }
+            else
+            {
+                return Json(new { success = false, status =" Opercja zapisu zatrzymana" });
+            }
+        }
+        private bool Porownanie(Podstawowa arch)
+        {
+            var podstawowa = Podstawowa.GetById(arch.id);
+            bool q = podstawowa.id.Equals(arch.id);
+            if (!q) return false;
+            q = podstawowa.nazwa.Equals(arch.nazwa);
+            if (!q) return false;
+            q = podstawowa.dataPoczatek.Equals(arch.dataPoczatek);
+            if (!q) return false;
+            q = podstawowa.dataKoniec.Equals(arch.dataKoniec);
+            if (!q) return false;
+            q = podstawowa.osobaOdpowiedzialna.Equals(arch.osobaOdpowiedzialna);
+            if (!q) return false;
+            q = podstawowa.typ.Equals(arch.typ);
+            if (!q) return false;
+            return true;
+        }
+    }
+  
+}
+
---
+++ WebIPG/WebIPG/Models/Podstawowa.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebIPG.Models
+{
+    interface IServicePodstawowa
+    {
+        List<Podstawowa> GetAll();
+        Podstawowa GetById(int value);
+        void Update(Podstawowa value);
+    }
+    public class Podstawowa
+    {
+        public int id { get; set; }
+        public string nazwa { get; set; }
+      
+      
+        public DateTime dataPoczatek { get; set; }
+        public DateTime dataKoniec { get; set; }
+        public string osobaOdpowiedzialna { get; set; }
+        public int? typ { get; set; }
+    }
+    public class ServicePodstawowa : IServicePodstawowa
+    {
+        public List<Podstawowa> GetAll()
+        {
+            SqlConnection con = null;
+            List<Podstawowa> result = new List<Podstawowa>();
+            try
+            {
+                con = new SqlConnection(ConfigurationManager.ConnectionStrings["mycon"].ToString());
+                SqlCommand cmd = new SqlCommand("AllPodstawowa", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Podstawowa podstawowa = new Podstawowa();
+                    podstawowa.id = (int)reader[0];
+                    podstawowa.nazwa = reader[1].ToString();
+                    podstawowa.dataPoczatek=(DateTime)reader[2];
+                    podstawowa.dataKoniec = (DateTime)reader[3];
+                    podstawowa.osobaOdpowiedzialna = reader[4].ToString();
+                    podstawowa.typ = (int)reader[5];
+                    result.Add(podstawowa);
+                }
+                return result;
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        public Podstawowa GetById(int value)
+        {
+            SqlConnection con = null;
+            Podstawowa podstawowa = new Podstawowa();
+            try
+            {
+                con = new SqlConnection(ConfigurationManager.ConnectionStrings["mycon"].ToString());
+                SqlCommand cmd = new SqlCommand("PodstawowaID", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@id", value);
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+    
+                    podstawowa.id = (int)reader[0];
+                    podstawowa.nazwa = reader[1].ToString();
+                    podstawowa.dataPoczatek = (DateTime)reader[2];
+                    podstawowa.dataKoniec = (DateTime)reader[3];
+                    podstawowa.osobaOdpowiedzialna = reader[4].ToString();
+                    podstawowa.typ = (int)reader[5];
+                }
+                return podstawowa;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        public void Update(Podstawowa value)
+        {
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection(ConfigurationManager.ConnectionStrings["mycon"].ToString());
+                SqlCommand cmd = new SqlCommand("UpdatePodstawowa", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@id", value.id);
+                cmd.Parameters.AddWithValue("@nazwa", value.nazwa);
+                cmd.Parameters.AddWithValue("@dataPoczatek", Convert.ToDateTime( value.dataPoczatek));
+                cmd.Parameters.AddWithValue("@dataKoniec", Convert.ToDateTime (value.dataKoniec));
+                cmd.Parameters.AddWithValue("@osobaOdpowiedzialna", value.osobaOdpowiedzialna);
+                cmd.Parameters.AddWithValue("@typ", value.typ);
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
